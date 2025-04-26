5019f5dd55e523dafa267951922a331a28ba036b
leidy <yurleidyecheverriatanaka@gamil.com>
2025-04-26 12:41:19 -0500

    primer cambio

---
+++ hola.py
@@ -0,0 +1,16 @@
+print("hola mundo")
+
+print(2)
+
+prueba= 2
+prueba = 5
+numero_1 = '3'
+numero_2 = '5'
+
+prueba = (numero_1 + numero_2) #aca estamos sumando prueba 1 y numero2
+'''estos son para hacer una explicacion mas extensa'''
+
+texto = "soy programador"
+
+print(type(texto))
+print(type(prueba))
