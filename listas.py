c3e31e13a631e27226bb685175e0897478297c20
estiven <duvanestivensanchezcardona8@gmail.com>
2025-05-03 13:43:31 -0500

    aprendiendo sobre listas

--- listas.py
+++ listas.py
@@ -1,1 +1,18 @@
-#este espaco es para hablar de listas
+
+meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+#imprimir lista de meses
+print(meses)
+meses.append("lunes")
+print(meses)
+del (meses[-1])
+print(meses)
+len(meses)
+print(len(meses))
+meses.clear()
+print(meses)
+del meses
+
+meses1 = []
+meses1.append("enero")
+print(meses1)
+
